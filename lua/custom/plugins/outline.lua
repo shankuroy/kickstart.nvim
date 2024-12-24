fa98310589b2e5569763baeec40d587b9d32dc3f
shankuroy <64715768+shankuroy@users.noreply.github.com>
2024-12-25 00:20:29 +1100

    add outline plugin

---
+++ lua/custom/plugins/outline.lua
@@ -0,0 +1,13 @@
+-- https://github.com/hedyhli/outline.nvim
+
+return {
+  {
+    'hedyhli/outline.nvim',
+    lazy = true,
+    cmd = { 'Outline', 'OutlineOpen' },
+    keys = { -- Example mapping to toggle outline
+      { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    },
+    opts = {},
+  },
+}
